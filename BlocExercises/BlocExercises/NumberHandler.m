0aa92f089c2ab64a493911e7ad46af2f75614eb4
Julicia James <d.james128@gmail.com>
2014-09-03 22:04:28 -0400

    Completed simple data types and objects

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,55 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    //NSNumber is a container. it's an obj not a simple type.
+    
+    
+    NSInteger unboxedNumber = [number integerValue];
+    NSInteger twiceNumber = unboxedNumber * 2;
+    
+    return [NSNumber numberWithInteger:twiceNumber];
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @[];
+    
+    NSInteger smaller;
+    NSInteger larger;
+    
+    if (number > otherNumber) {
+        smaller = otherNumber;
+        larger = number;
+        
+    } else {
+        smaller = number;
+        larger = otherNumber;
+    }
+
+    NSMutableArray *arrayOfNumbersSmallToLarge = [[NSMutableArray alloc] init];
+    for (NSInteger i = smaller; i<=larger; i++) {
+        [arrayOfNumbersSmallToLarge addObject:[NSNumber numberWithInteger:i]];
+    }
+    
+    return arrayOfNumbersSmallToLarge;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
+    //only NSNumbers go in arrays. Integers don't go in an array.
+    //no star * for simple data types
+    
+    NSInteger minNumber = INT_MAX;
+    
+    for (NSNumber *number in arrayOfNumbers) {
+        NSInteger unboxedInteger = [number integerValue];
+        if (unboxedInteger < minNumber) {
+            minNumber = unboxedInteger;
+        }
+    }
+    
+    
+    
+    return minNumber;
 }
 
 @end
